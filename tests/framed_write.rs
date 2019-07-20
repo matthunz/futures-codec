4a836fe4c14aef3b10f4179df21282981e117960
Colin S <3526918+cbs228@users.noreply.github.com>
2019-07-20 15:29:42 -0500

    Proof test for bottomless streams
    Sink::send_all() will attempt to read the entire stream into the
    FramedWrite and send it all at once, at the conclusion of the
    read. This patch includes a test which demonstrates this behavior.

--- tests/framed_write.rs
+++ tests/framed_write.rs
@@ -1,7 +1,58 @@
-use futures::executor;
+use bytes::Bytes;
+use core::iter::Iterator;
+use futures::io::AsyncWrite;
 use futures::sink::SinkExt;
-use futures_codec::{FramedWrite, LinesCodec};
+use futures::task::Context;
+use futures::Poll;
+use futures::{executor, stream};
+use futures_codec::{BytesCodec, FramedWrite, LinesCodec};
 use std::io::Cursor;
+use std::pin::Pin;
+
+// An iterator which outputs a single zero byte up to limit times
+struct ZeroBytes {
+    pub count: usize,
+    pub limit: usize,
+}
+impl Iterator for ZeroBytes {
+    type Item = Bytes;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count >= self.limit {
+            None
+        } else {
+            self.count += 1;
+            Some(Bytes::from_static(b"\0"))
+        }
+    }
+}
+
+// An AsyncWrite which is always ready and just consumes the data
+struct AsyncWriteNull {
+    // number of poll_write calls
+    pub num_poll_write: usize,
+
+    // size of the last poll_write
+    pub last_write_size: usize,
+}
+impl AsyncWrite for AsyncWriteNull {
+    fn poll_write(
+        mut self: Pin<&mut Self>,
+        _cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<std::io::Result<usize>> {
+        self.num_poll_write += 1;
+        self.last_write_size = buf.len();
+        Poll::Ready(Ok(buf.len()))
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn poll_close(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+        Poll::Ready(Ok(()))
+    }
+}
 
 #[test]
 fn line_write() {
@@ -23,4 +74,23 @@ fn line_write_to_eof() {
     let (curs, _) = framer.release();
     assert_eq!(curs.position(), 16);
     assert_eq!(&curs.get_ref()[0..16], b"This will fill u");
+}
+
+#[test]
+fn really_big_writes() {
+    const NUM_WRITE: usize = 524288;
+
+    let io = AsyncWriteNull {
+        num_poll_write: 0,
+        last_write_size: 0,
+    };
+    let mut framer = FramedWrite::new(io, BytesCodec {});
+    let mut stream = stream::iter(ZeroBytes {
+        count: 0,
+        limit: NUM_WRITE,
+    });
+    executor::block_on(framer.send_all(&mut stream)).unwrap();
+    let (io, _) = framer.release();
+    assert_eq!(io.num_poll_write, 1);
+    assert_eq!(io.last_write_size, NUM_WRITE);
 }
